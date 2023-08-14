9f1dd46123ee8ef6f30051e2769f8f33e9c09ed6
Jakob Strasser <97665596+JakobStrasser@users.noreply.github.com>
2023-08-14 10:37:13 +0200

    Added new board generation from dosudoku

---
+++ Shared/ApiResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace Sudoku.Shared
+{
+    public class ApiResponse
+    {
+        public class Grid
+        {
+            public List<List<int>> value { get; set; }
+            public List<List<int>> solution { get; set; }
+            public string difficulty { get; set; }
+        }
+
+        public class Newboard
+        {
+            public List<Grid> grids { get; set; }
+            public int results { get; set; }
+            public string message { get; set; }
+        }
+
+        public class Root
+        {
+            public Newboard newboard { get; set; }
+        }
+    }
+
+}
